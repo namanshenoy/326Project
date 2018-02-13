9f53b90a28d315fd2624e8cd847180b12ff9161b
namanshenoy <naman.skillet@gmail.com>
2018-02-13 16:37:41 -0500

    More gitignore stuff for vs code, Hello world

--- MockUI/MockUI/urls.py
+++ MockUI/MockUI/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import Site.views as site_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', site_views.home)
 ]
