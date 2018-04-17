8a1ac7c9e05f21decfc2173a73ab767c866fd9cb
Corey Collins <cwcollins@umass.edu>
2018-04-17 00:04:22 -0400

    included names in comments

--- MockUI/Site/serializers.py
+++ MockUI/Site/serializers.py
@@ -9,9 +9,16 @@ from rest_framework.serializers import (
 from .models import *
 from django.contrib.auth import get_user_model
 from django.db.models import Q
+from django.contrib.auth.models import User
 
+
+class UserInfoSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name')
 
 class CommentInfoSerializer(ModelSerializer):
+    user = UserInfoSerializer(read_only=True, many=False)
     class Meta:
         model = Comment
         fields = ('id', 'user', 'data', 'date_time')
